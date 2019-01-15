21954f96974799821a8166f7ecd7e6e5967d3f25
joblackpoc <duopoc@gmail.com>
2019-01-15 14:54:03 +0700

    add project

--- budget/urls.py
+++ budget/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.project_list, name='list'),
+    path('add', views.ProjectCreateView.as_view(), name='add'),
     path('<slug:project_slug>', views.project_detail, name='detail'),
 ]
--- budget/views.py
+++ budget/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import render, get_object_or_404
 from .models import Project
+from django.views.generic import CreateView
 
 
 def project_list(request):
@@ -9,3 +10,8 @@ def project_list(request):
 def project_detail(request, project_slug):
     project = get_object_or_404(Project, slug=project_slug)
     return render(request, 'budget/project-detail.html', {'project': project, 'expense_list': project.expenses.all()})
+
+class ProjectCreateView(CreateView):
+    model = Project
+    template_name = 'budget/add-project.html'
+    fields = ('name', 'budget')
