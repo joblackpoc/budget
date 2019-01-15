e3b708cf673a8bb6135f75c81cf7796236fff9f9
joblackpoc <duopoc@gmail.com>
2019-01-15 13:23:26 +0700

    make migrations

--- budget/admin.py
+++ budget/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Project, Expense, Category
+
+admin.site.register(Project)
+admin.site.register(Expense)
+admin.site.register(Category)
 
 # Register your models here.
--- budget/models.py
+++ budget/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
+    budget = models.IntegerField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Project, self).save(*args, **kwargs)
+
+class Category(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+class Expense(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
