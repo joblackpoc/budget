7ca408bcd6ed12c460b7b1ba725cbad4a5b055ca
joblackpoc <duopoc@gmail.com>
2019-01-23 11:28:26 +0700

    function remove scritp.js

--- budget/static/js/script.js
+++ budget/static/js/script.js
@@ -23,21 +23,27 @@
     </li>`)
   }
 
-  function fetchCategoryArray(){
-    var categories = []
 
-    document.querySelectorAll('.category').forEach(function(e){
-      name = e.querySelector('.name').innerHTML
-      if (name == '') return;
-      
-      categories.push(name)
-    })
-
-    return categories
-  }
-
-  function updateCategoriesString(){
-    categories = fetchCategoryArray()
-    document.querySelector('input[name="categoriesString"]').value = categories.join(',')
-  }
 })()
+function fetchCategoryArray(){
+  var categories = []
+
+  document.querySelectorAll('.category').forEach(function(e){
+    name = e.querySelector('.name').innerHTML
+    if (name == '') return;
+    
+    categories.push(name)
+  })
+
+  return categories
+}
+
+function updateCategoriesString(){
+  categories = fetchCategoryArray()
+  document.querySelector('input[name="categoriesString"]').value = categories.join(',')
+}
+
+function removeCategory(e){
+  e.parentElement.remove()
+  updateCategoriesString()
+}
