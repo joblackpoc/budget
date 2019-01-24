bad98f910888004992a5a412c67251440d842e44
joblackpoc <duopoc@gmail.com>
2019-01-24 11:09:10 +0700

    finish Add Project

--- budget/views.py
+++ budget/views.py
@@ -1,7 +1,9 @@
 
 from django.shortcuts import render, get_object_or_404
-from .models import Project
+from django.http import HttpResponseRedirect
+from .models import Project, Category
 from django.views.generic import CreateView
+from django.utils.text import slugify
 
 
 def project_list(request):
@@ -15,3 +17,19 @@ class ProjectCreateView(CreateView):
     model = Project
     template_name = 'budget/add-project.html'
     fields = ('name', 'budget')
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+
+        categories = self.request.POST['categoriesString'].split(',')
+        for category in categories:
+            Category.objects.create(
+                project=Project.objects.get(id=self.object.id),
+                name=category
+            ).save()
+
+        return HttpResponseRedirect(self.get_success_url())
+    
+    def get_success_url(self):
+        return slugify(self.request.POST['name'])
