d15b48a2b8d5650955c41827d294dc4c4976ae15
joblackpoc <duopoc@gmail.com>
2019-01-28 11:42:37 +0700

    modified Project list

--- budget/urls.py
+++ budget/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.project_list, name='list'),
+    path('admin', admin.site.urls),
     path('add', views.ProjectCreateView.as_view(), name='add'),
     path('<slug:project_slug>', views.project_detail, name='detail')
 ]
